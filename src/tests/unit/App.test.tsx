b708b30f89920aea8cb346061bfadfecd7d842ff
Mariana Costa <43031902+marianapatcosta@users.noreply.github.com>
2022-06-11 23:21:07 +0100

    setup cypress

--- src/tests/unit/App.test.tsx
+++ src/tests/unit/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App from '../../App';
 
 describe('App', () => {
   it('should render without errors and match snapshot', () => {
